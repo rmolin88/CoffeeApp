836970d1c0824d09a114b25dc167a43d5f191fa0
Reinaldo Molina <rmolin88@gmail.com>
2016-09-01 18:08:25 -0400

    first working state of app

--- app/src/main/java/com/hq/project1/MainActivity.java
+++ app/src/main/java/com/hq/project1/MainActivity.java
@@ -2,18 +2,42 @@ package com.hq.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
-import android.widget.TextWatcher;
+import android.util.Log;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
+  private static final String TAG = "CoffeeApp";
+
   private EditText etNumCoffee;
   private TextView tvFinalPrice;
+  private final TextWatcher twNumCoffee =
+      new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence ch, int start, int count, int after) {}
+
+        @Override
+        public void beforeTextChanged(CharSequence ch, int start, int count, int after) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {
+          try {
+            Log.i(TAG, "afterTextChanged got called");
+            if (s.length() == 0) NumCoffee = 0;
+            else NumCoffee = Integer.parseInt(s.toString());
+            UpdateTotalPrice();
+          } catch (Exception e) {
+            NumCoffee = 0;
+          }
+        }
+      };
 
   private static int NumCoffee = 0;
   private String sPrice;
@@ -25,7 +49,12 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    Log.i(TAG, "Initializing");
     etNumCoffee = (EditText) findViewById(R.id.edit_text_order_quantity);
+    //    Connect the listener to the text view
+    Log.i(TAG, "Adding TextChangeListener");
+    etNumCoffee.addTextChangedListener(twNumCoffee);
+    Log.i(TAG, "TextChangeListener was added");
     tvFinalPrice = (TextView) findViewById(R.id.text_total_amt);
 
     // Convert Price @string/price_val to int
@@ -35,29 +64,23 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void AddBtnAction(View view) {
+    Log.i(TAG, "Add Btn got called");
     NumCoffee++;
-    UpdateParams();
+    UpdateNumCoffeeDisp();
   }
 
   public void SubBtnAction(View view) {
     if (NumCoffee == 0) return;
     NumCoffee--;
-    UpdateParams();
+    UpdateNumCoffeeDisp();
   }
 
-  private void UpdateParams() {
+  private void UpdateNumCoffeeDisp() {
     etNumCoffee.setText("" + NumCoffee);
+  }
+
+  private void UpdateTotalPrice() {
     iTotal = iPrice * NumCoffee;
     tvFinalPrice.setText("$" + iTotal);
-  }
-
-  // looking into textwatcher
-  public void OrderQtyAction(View view){
-    try {
-      NumCoffee = Integer.parseInt(etNumCoffee.getText().toString());
-    } catch(Exception e) {
-      NumCoffee = 0;
-    }     
-    UpdateParams();
   }
 }
